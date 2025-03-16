994ef7427a7828f3d47d193aad4b1e49762d566f
Marco Tello <marcotello5@outlook.com>
2025-03-16 01:22:08 -0600

    Adding the updateCurrentConditionsForLocation method that will update the data for a zipcode

--- src/app/store/location.store.ts
+++ src/app/store/location.store.ts
@@ -1,12 +1,12 @@
-import {patchState, signalStore, withHooks, withMethods, withState} from '@ngrx/signals';
+import {patchState, signalStore, withComputed, withHooks, withMethods, withState} from '@ngrx/signals';
 import {ConditionsAndZip} from '../model/conditions-and-zip.type';
-import {effect, inject} from '@angular/core';
+import {computed, effect, inject} from '@angular/core';
 import {LocationStorageService} from '../services/location-storage.service';
 import {rxMethod} from '@ngrx/signals/rxjs-interop';
 import {tapResponse} from '@ngrx/operators';
 import {WeatherService} from '../services/weather.service';
 import {pipe} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
+import {mergeMap, switchMap} from 'rxjs/operators';
 import {CurrentConditions} from '../model/current-conditions.type';
 
 interface LocationState {
@@ -20,15 +20,11 @@ const initialState: LocationState = {
 export const LocationStore = signalStore(
     { providedIn: 'root' },
     withState(initialState),
-    withMethods((store, locationStorageService = inject(LocationStorageService)) => ({
-        // private methods
-        _loadAllLocations() {
-            const locations = locationStorageService.getLocationsFromStorage();
-            patchState(store, { locations });
-        }
+    withComputed((state) => ({
+        zipCodes: computed(() => state.locations().map(location => location.zip))
     })),
     withMethods((store, weatherService = inject(WeatherService)) => ({
-        addLocation: rxMethod<string>(
+        addLocation: rxMethod<string> (
             pipe(
                 switchMap((zipcode) => {
                     return weatherService.addCurrentConditions(zipcode).pipe(
@@ -48,12 +44,51 @@ export const LocationStore = signalStore(
             patchState(store, (state) => ({
                 locations: state.locations.filter((location) => location.zip !== zipcode)
             }));
+        },
+        updateCurrentConditionsForLocation: rxMethod<string> (
+            pipe(
+                mergeMap((zipcode) => {
+                    return weatherService.addCurrentConditions(zipcode).pipe(
+                        tapResponse({
+                            next: (currentConditions: CurrentConditions) => {
+                                const locationIndex = store.locations().findIndex(location => location.zip === zipcode);
+
+                                patchState(store, (state) => ({
+                                    locations: [...state.locations.slice(0, locationIndex),
+                                        { zip: zipcode, data: currentConditions },
+                                        ...state.locations.slice(locationIndex + 1)]
+                                }));
+                            },
+                            error: (error) => console.error(error)
+                        })
+                    );
+                })
+            )
+        )
+    })),
+    withMethods((store, locationStorageService = inject(LocationStorageService)) => ({
+        // private methods
+        _loadLocationsFromStorage() {
+            const locations = locationStorageService.getLocationsFromStorage();
+            patchState(store, { locations });
+        },
+        _fetchCurrentConditionsForLocations() {
+            for (const zipcode of store.zipCodes()) {
+                store.updateCurrentConditionsForLocation(zipcode);
+            }
         }
+    })),
+    withMethods((store) => ({
+        loadAllLocations() {
+            store._loadLocationsFromStorage();
+            store._fetchCurrentConditionsForLocations();
+        },
     })),
     withHooks({
         onInit(store, locationStorageService = inject(LocationStorageService)) {
-            effect(() => locationStorageService.saveLocationsToStorage(store.locations()))
-            store._loadAllLocations()
+            effect(() => locationStorageService.saveLocationsToStorage(store.locations()));
+
+            store.loadAllLocations();
         }
     })
 );
