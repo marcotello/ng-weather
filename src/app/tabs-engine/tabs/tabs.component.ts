d376a9f795f758e232d20045aa1afeb17683011b
Marco Tello <marcotello5@outlook.com>
2025-03-19 22:10:59 -0600

    Implanting the tabs structure and the header dynamic rendering

--- src/app/tabs-engine/tabs/tabs.component.ts
+++ src/app/tabs-engine/tabs/tabs.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, ContentChildren, QueryList} from '@angular/core';
+import {TabComponent} from '../tab/tab.component';
+import {NgForOf, TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    TitleCasePipe
+  ],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.css'
 })
-export class TabsComponent {
+export class TabsComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
 
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs.filter(tab => tab.isActive);
+
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs.toArray().forEach(tab => tab.isActive = false);
+
+    tab.isActive = true;
+  }
+
+  closeTab(tabId: string) {
+    console.log('Tab closed ' + tabId);
+  }
 }
